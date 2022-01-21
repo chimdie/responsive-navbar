66007a4fc7023e14f0eaf74ff4973ba9f07d6969
chimdi nze <nzechimdi@yahoo.com>
2022-01-21 10:55:54 +0100

    removes unused lines

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Navbar from './components/Navbar'
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Switch>
-        <Route exact path='/' />
+        <Route exact path="/" />
       </Switch>
     </Router>
   );
